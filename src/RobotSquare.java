9e00c4880a57aa57afa5f4051d1ac7066c7a2a24
49071 <49071@0338S-S-R0B6ZHC.sdcs.local>
2018-10-09 14:28:24 -0700

    robot square

--- src/RobotSquare.java
+++ src/RobotSquare.java
@@ -12,13 +12,14 @@ public class RobotSquare {
  
     	// 1. Make a new Robot
 
+Robot rd2d = new Robot();
 
         // 3. Put the robot's pen down
-
+rd2d.penDown();
 
         // 6. Make the robot move as fast as possible
 
-
+rd2d.setSpeed(15);
         // 5. Do everything below here 4 times
 
 
@@ -27,6 +28,11 @@ public class RobotSquare {
 
         //         4. Turn the robot 90 degrees to the right (90 degrees)
 
-
+for (int i = 0; i <8  ; i++) {
+	
+	rd2d.move(100);
+	rd2d.turn(360/8);
+}
     }
+    
 }
