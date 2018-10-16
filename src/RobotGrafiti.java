690037cc29f218b187dc3daeaf4b92aa941cdd32
49071 <49071@0338S-S-R0B6ZHC.sdcs.local>
2018-10-16 14:22:41 -0700

    name

---
+++ src/RobotGrafiti.java
@@ -0,0 +1,53 @@
+import org.jointheleague.graphical.robot.Robot;
+
+public class RobotGrafiti {
+    public static void main(String[] args) throws Exception {
+
+ Robot L1 = new Robot();
+ L1.penDown();
+ 
+ L1.turn(90);
+ 
+ L1.setSpeed(13);
+ 
+ L1.move(200);
+ 
+ L1.turn(180);
+
+
+ L1.move(200);
+ 
+ L1.turn(90);
+ 
+ L1.move(250);
+ 
+ L1.penUp();
+ 
+ L1.turn(90);
+ 
+ L1.move(250);
+ 
+ L1.penDown();
+ 
+ L1.turn(90);
+ L1.move(250);
+ 
+ L1.turn(270);
+ 
+ L1.move(200);
+ 
+ L1.turn(180);
+ 
+ 
+ 
+
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+    }
+
+}
